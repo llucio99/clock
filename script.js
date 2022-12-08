4624c846c741308c6cbc3aca3718e4d7c599abc1
llucio99 <llucio99@icloud.com>
2022-12-07 23:08:25 -0800

    Import script into HTML

---
+++ script.js
@@ -0,0 +1,21 @@
+var hour = document.querySelector(".hour");
+var minute = document.querySelector(".minute");
+var second = document.querySelector(".second");
+
+function time() {
+    var time = new Date();
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    var hoursAngle = (hours / 12) * 360;
+    var minutesAngle = (minutes / 60) * 360;
+    var secondsAngle = (seconds / 60) * 360;
+
+    hour.style.transform = `rotate(${hoursAngle}deg)`;
+    minute.style.transform = `rotate(${minutesAngle}deg)`;
+    second.style.transform = `rotate(${secondsAngle}deg)`;
+}
+
+setInterval(time, 1000);
